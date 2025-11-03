ea092ebc81e0ff16b3e4e41f27735fd2c31e1f16
s22a0061 <s22a0061@siswa.umk.edu.my>
2025-11-04 00:07:47 +0800

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -24,7 +24,7 @@ program_ratings_dict = read_csv_to_dict(file_path)
 
 # Print the result (you can also return or process it further)
 for program, ratings in program_ratings_dict.items():
-    st.write(f"'{program}': {ratings},")
+    print(f"'{program}': {ratings},")
 
 
 import random
